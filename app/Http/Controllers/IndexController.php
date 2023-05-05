cbc5b30900dc1673a6633b2f3036c163bff865b0
Arthur Warmling <arthurwarmling1@gmail.com>
2023-05-05 12:40:11 -0300

    first commit

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Imports\ServersImport;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $path = storage_path('/app/db/servers.xlsx');
+        $reader = IOFactory::createReader('Xlsx');
+        $reader = $reader->setLoadSheetsOnly('Sheet2');
+        $sheet = $reader->load($path);
+
+        $servers = $sheet->getActiveSheet()->toArray();
+        unset($servers[0]);
+
+        $data = [];
+        $locations = [];
+        $id = 1;
+        foreach ($servers as $item) {
+            $ram = $this->getRamSizeAndType($item[1]);
+            $storage = $this->getStorageSizeAndType($item[2]);
+
+            $data[] = [
+                'id' => $id,
+                'model' => $item[0],
+                'ram' => $ram['size'],
+                'ram_type' => $ram['type'],
+                'storage' => $item[2],
+                'location' => $item[3],
+                'price' => $item[4],
+                'converted_storage_gb' => $storage['converted_storage_gb'],
+                'converted_storage_unity' => $storage['unity'],
+                'storage_type' => $storage['storage_type'],
+            ];
+
+            $id++;
+            $locations[] = $item[3];
+        }
+
+        $locations = array_unique($locations);
+
+        dd($locations, $data);
+    }
+
+    private function getRamSizeAndType(string $ram)
+    {
+        if (empty($ram)) {
+            return [];
+        }
+
+        $find = 'B';
+        $position = strpos($ram, $find) + 1;
+        $stringLength = strlen($ram);
+
+        return [
+            'size' => substr($ram, 0, $position),
+            'type' => substr($ram, $position, $stringLength)
+        ];
+    }
+
+    private function getStorageSizeAndType(string $storage)
+    {
+        if (empty($storage)) {
+            return [];
+        }
+
+        $values = ['0', '250GB', '500GB', '1TB', '2TB', '3TB', '4TB', '8TB', '12TB', '24TB', '48TB', '72TB'];
+        $memoryInfo = explode('x', $storage);
+
+        $quantityStorage = $memoryInfo[0];
+        $quantityStorageAndType = $memoryInfo[1];
+
+        $position = strpos($quantityStorageAndType, 'B') + 1;
+        $quantityMemory = substr($quantityStorageAndType, 0, $position);
+        $storageType = str_replace($quantityMemory, '', $quantityStorageAndType);
+
+        $quantityRamInt = preg_replace('/[^0-9]/', '', $quantityMemory);
+        $totalMemory = intval($quantityRamInt) * intval($quantityStorage);
+        $unityMemory = preg_replace("/[^A-Z]+/", "", $quantityMemory);
+        $storageType = preg_replace("/[^A-Z]+/", "", $storageType);
+
+        if ($unityMemory == 'TB') {
+            $totalMemory = $totalMemory * 1000;
+        }
+
+        return [
+            'converted_storage_gb' => $totalMemory,
+            'unity' => $unityMemory,
+            'storage_type' => $storageType
+        ];
+    }
+}
